d210376a1b68d2fad1a788279e1740ed81640e75
NicolasD2 <nicolas.diaz03@alumnos.ucn.cl>
2025-12-09 23:44:29 -0400

    feat: create user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import {v4 as uuidv4} from 'uuid';
+import {User, UserPublicData} from './interfaces/user.interfaces';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    private users: User[] = [
+        {
+            id: uuidv4(),
+            fullName: 'David Araya',
+            email: 'daraya@ucn.cl',
+            username: 'daraya',
+            birthdate: new Date('1985-01-01'),
+            address: 'Calle Falsa 123',
+            phoneNumber: '555-1234',
+            passwordHash: 'hashedpassword1',
+            status: 'active'
+        },
+    ];
+
+    private toPublicData(user: User): UserPublicData {
+        const { passwordHash, ...publicData } = user;
+        return publicData;
+    }
+}
