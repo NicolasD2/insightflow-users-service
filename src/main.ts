ae87827e383e73a682fca913717bbe08908b9600
NicolasD2 <nicolas.diaz03@alumnos.ucn.cl>
2025-12-14 03:00:31 -0400

    feat: enable CORS for local development

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
   const port = process.env.PORT || 3000;
   await app.listen(port);
 }
